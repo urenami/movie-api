a8ca1740ecc902f42074b0eeb4127081dff8153b
urenami <122377715+urenami@users.noreply.github.com>
2023-04-22 12:54:56 -0400

    added mongoose, updated index.js, and documentation.html

--- movie-api/index.js
+++ movie-api/index.js
@@ -4,6 +4,13 @@ const express = require('express'),
         path = require('path'),
         uuid = require('uuid'),
         bodyParser = require('body-parser');
+        const mongoose = require('mongoose');
+        const Models = require('./models.js');
+
+        const Movies = Models.Movie;
+        const Users = Models.User;
+
+        mongoose.connect('mongodb://localhost:27017/cfDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 // declaring that variable app = deploy express() function
@@ -20,200 +27,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
 
-//My top 10 Movies
-let movies = [
-  { 
-          Title: 'The Dark Knight',
-          Description: 'The Batman, police lieutenant James Gordon, and district attorney Harvey Dent, who form an alliance to dismantle organized crime in Gotham City.' ,
-          Genre: 
-          {
-            Name: 'Action',
-            Description: 'Action film is a film genre in which the protagonist is thrust into a series of events that typically involve violence and physical feats.'
-    },
-          Director:
-    {
-            Name: 'Christopher Nolan',
-            bio: 'Christopher Edward Nolan CBE is a British-American filmmaker. Known for his Hollywood blockbusters with complex storytelling. Nolan is considered a leading filmmaker of the 21st century.' ,
-            Birthyear: '1970'
-    },
-            imageUrl:'https://www.imdb.com/title/tt0468569/mediaviewer/rm4023877632/?ref_=tt_ov_i',
-            year: '2008'
-  },
-
-  { 
-          Title: 'The Dark Knight Rises',
-          Description: 'The revolutionary Bane forces Bruce Wayne to resume his role as Batman and save Gotham City from nuclear destruction.' ,
-          Genre:
-    {
-            Name: 'Action',
-            Description: 'Action film is a film genre in which the protagonist is thrust into a series of events that typically involve violence and physical feats.' 
-    },
-          Director:
-    {
-            Name: 'Christopher Nolan',
-            bio: 'Christopher Edward Nolan CBE is a British-American filmmaker. Known for his Hollywood blockbusters with complex storytelling. Nolan is considered a leading filmmaker of the 21st century.' ,
-            Birthyear: '1970'
-    },
-            imageUrl:'https://www.imdb.com/title/tt1345836/mediaviewer/rm834516224/?ref_=tt_ov_i',
-            year: '2012'
-  },  
-  {
-          Title: 'Coming To America',
-          Description: 'The crown prince of the fictional African nation of Zamunda, who travels to the United States in the hopes of finding a woman he can marry and love for who she is, not for her status or for having been trained to please him.' ,
-          Genre:
-    {
-            Name: 'Comedy',
-            Description: 'Comedy is a genre of fiction that consists of discourses or works intended to be humorous or amusing by inducing laughter.' ,
-    },
-          Director: 
-    {
-            Name: 'John Landis',
-            bio: 'John David Landis is an American comedy, horror, and fantasy filmmaker and actor. He is best known for the comedy films that he has directed.' ,
-            Birthyear: '1970'
-    },
-            imageUrl: 'https://www.imdb.com/title/tt0094898/mediaviewer/rm2778142208/?ref_=tt_ov_i',
-            year: '1988'
-  },
-  {
-          Title: 'Trading Places',
-          Description: 'The story of an upper-class commodities broker (Aykroyd) and a poor street hustler (Murphy) whose lives cross when they are unwittingly made the subject of an elaborate bet to test how each man will perform when their life circumstances are swapped.',
-          Genre:
-    {
-          Name: 'Comedy',
-          description: 'Comedy is a genre of fiction that consists of discourses or works intended to be humorous or amusing by inducing laughter.',
-    },      
-          Director: 
-    {
-            Name: 'John Landis',
-            bio: 'John David Landis is an American comedy, horror, and fantasy filmmaker and actor. He is best known for the comedy films that he has directed.' ,
-            Birthyear: '1970'
-    },
-            imageUrl:'https://www.imdb.com/title/tt0086465/mediaviewer/rm3049141505/?ref_=tt_ov_i',
-            year:'1983'
-  },
-  {
-          Title: 'Thor',
-          Description: 'After reigniting a dormant war, Thor is banished from Asgard to Earth, stripped of his powers and his hammer. As his brother Loki (Hiddleston) plots to take the Asgardian throne, Thor must prove himself worthy.',
-          Genre:
-    {
-          Name: 'Action',
-          Description: 'Action film is a film genre in which the protagonist is thrust into a series of events that typically involve violence and physical feats.',
-    },
-          Director:
-    {
-            Name: 'Kenneth Branagh',
-            bio: 'Joseph Hill Whedon is an American filmmaker, composer, and comic book writer. He is the founder of Mutant Enemy Productions, co-founder of Bellwether Pictures, and is best known as the creator of several television series.' ,
-            Birthyear: '1960'
-    },
-            imageUrl: 'https://www.imdb.com/title/tt0800369/mediaviewer/rm3272546304/?ref_=tt_ov_i',
-            year: '2011'
-  },
-  {
-            Title: 'The Avengers',
-            Description: 'Nick Fury and the spy agency S.H.I.E.L.D. recruit Tony Stark, Steve Rogers, Bruce Banner, Thor, Natasha Romanoff, and Clint Barton to form a team capable of stopping Thors brother Loki from subjugating Earth.',
-            Genre:
-    {
-            Name: 'Action',
-            description: 'Action film is a film genre in which the protagonist is thrust into a series of events that typically involve violence and physical feats.',
-    },
-          Director: 
-    {
-            Name: 'Joss Whedon',
-            bio: 'John David Landis is an American comedy, horror, and fantasy filmmaker and actor. He is best known for the comedy films that he has directed.' ,
-            Birthyear: '1964'
-    },
-            imageUrl: 'https://www.imdb.com/title/tt0848228/mediaviewer/rm3955117056/?ref_=tt_ov_i',
-            year: '2012'
-  },
-  {
-          Title: 'Avengers: Infinity War',
-          Description: 'The Avengers and the Guardians of the Galaxy attempt to prevent Thanos from collecting the six all-powerful Infinity Stones as part of his quest to kill half of all life in the universe.',
-          Genre:
-    {
-            Name: 'Action',
-            Description: 'Action film is a film genre in which the protagonist is thrust into a series of events that typically involve violence and physical feats.',
-    },      
-          Director: 
-    {
-            Name: 'Anthony Russo, Joe Russo',
-            bio: 'Anthony Russo and Joseph Russo, collectively known as the Russo brothers, are American directors, producers, and screenwriters. They direct most of their work together. They are best known for directing four films in the Marvel Cinematic Universe.' ,
-            Birthyear: '1970, 1971'
-    },
-            imageUrl: 'https://www.imdb.com/title/tt4154756/mediaviewer/rm4044245504/?ref_=tt_ov_i',
-            year: '2018'
-  },
-  {
-          Title: 'Avengers: End Game ',
-          Description: 'The surviving members of the Avengers and their allies attempt to reverse Thanos actions in Infinity War.',
-          Genre:
-    {
-            Name: 'Action',
-            Description: 'Action film is a film genre in which the protagonist is thrust into a series of events that typically involve violence and physical feats.',
-    },      
-          Director: 
-    {
-            Name: 'Anthony Russo, Joe Russo',
-            bio: 'Anthony Russo and Joseph Russo, collectively known as the Russo brothers, are American directors, producers, and screenwriters. They direct most of their work together. They are best known for directing four films in the Marvel Cinematic Universe.' ,
-            Birthyear: '1970, 1971'
-    },
-            imageUrl: 'https://www.imdb.com/title/tt4154796/mediaviewer/rm2775147008/?ref_=tt_ov_i',
-            year: '2019'       
-  },
-  {
-          Title: 'Lucky Number Slevin',
-          Description: 'Revolves around an innocent man dragged into the middle of a war being plotted by two of New York Citys rival crime bosses.',
-          Genre:
-    {
-            Name: 'Crime',
-            Description: 'Crime films, in the broadest sense, is a film genre inspired by and analogous to the crime fiction literary genre.',
-    },      
-          Director: 
-    {
-            Name: 'Paul McGuigan',
-            bio: 'Paul McGuigan is a Scottish film and television director, best known for directing films such as Lucky Number Slevin, Gangster No. 1 and Push. He has also directed episodes of Sherlock, Scandal, Monroe and Smash.' ,
-            Birthyear: '1963'
-    },
-            imageUrl: 'https://www.imdb.com/title/tt0425210/mediaviewer/rm288767744/?ref_=tt_ov_i',
-            year: '2006'
-  },
-  {
-            Title: 'Scarface',
-            Description:'The story of Cuban refugee Tony Montana (Al Pacino), who arrives penniless in Miami during the Mariel boatlift and becomes a powerful and extremely homicidal drug lord.',
-            Genre:
-     {
-            Name: 'Crime',
-            Description: 'Crime films, in the broadest sense, is a film genre inspired by and analogous to the crime fiction literary genre.',
-
-     },       
-            Director: 
-      {
-              Name: 'Brian De Palma',
-              bio: 'Brian Russell De Palma is an American film director and screenwriter. With a career spanning over 50 years, he is best known for his work in the suspense, crime and psychological thriller genres.' ,
-              Birthyear: '1940'
-      },
-              imageUrl: 'https://www.imdb.com/title/tt0086250/mediaviewer/rm512766208/?ref_=tt_ov_i',
-              year: '1983'
-  },
-];
-
-let users = [
-        {
-            id: '1',
-            name: 'Mike',
-            favoriteMovieList: []
-        },
-        {
-            id: '2',
-            name: 'Ana',
-            favoriteMovieList: []
-        },
-        {
-            id: '3',
-            name: 'Michael',
-            favoriteMovieList: ["Avenger: End Game, The Dark Knight"]
-        }
-]
-
 //READ
 app.get('/documentation', (req, res) => {                  
         console.log('Documentation Request');
@@ -226,113 +39,162 @@ app.get('/', (req, res) => {
   res.send('Welcome to myFlix app!');
 });
 
+app.get('/users', (req, res) => {
+  Users.find()
+    .then((users) => {
+      res.status(201).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+app.get('/users/:Username', (req, res) => {
+  Users.findOne({ Username: req.params.Username })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
 app.get('/movies', (req, res) => {
-  res.status(200).json(movies);
-  console.log('Movies request');
+        Movies.find({ Movies: req.params.Movies })
+        .then((movies) => {
+          res.json(movies);
+        })
+        .catch((err) => {
+          console.error(err);
+          res.status(500).send('Error: ' + err);
+        });
 });
 
 //get request to get info on movie using title
-app.get('/movies/:title', (req, res) => {
-  const {title} = req.params;
-  const movie = movies.find(movie => movie.Title === title);
-
-  if (movie) {
-    res.status(200).json(movie);
-  } else{
-    res.status(400).send('no such movie')
-  }
+app.get('/movies/:Title', (req, res) => {
+        Movies.findOne({ Title: req.params.Title })
+        .then((movies) => {
+          res.json(movies);
+        })
+        .catch((err) => {
+          console.error(err);
+          res.status(500).send('Error: ' + err);
+        });
 });
 
 //get request to get genre of a movie
 app.get('/movies/genre/:genreName', (req, res) => {
-  const {genreName} = req.params;
-  const genre = movies.find(movie => movie.Genre.Name === genreName).Genre;
-
-  if (genre) {
-    res.status(200).json(genre);
-  } else{
-    res.status(400).send('no such genre')
-  }
+        Movies.find({ 'Genre.Name': req.params.genreName })
+        .then((movies) => {
+          res.json(movies);
+        })
+        .catch((err) => {
+          console.error(err);
+          res.status(500).send('Error: ' + err);
+        });
 });
 
 app.get('/movies/director/:directorName',(req,res)=>{
-  const {directorName} =req.params;
-  const director = movies.find(movie => movie.Director.Name === directorName).Director;
-
-  if (director){
-      res.status(200).json(director);
-  }else{
-      res.status(400).send('no such director found');
-  }
+        Movies.findOne({ 'Director.Name': req.params.directorName })
+        .then((movies) => {
+          res.json(movies.Director);
+        })
+        .catch((err) => {
+          console.error(err);
+          res.status(500).send('Error: ' + err);
+        });
 });
 
 //CREATE
 app.post('/users', (req,res) => {
-        const newUser = req.body;
-      
-        if (newUser.name) {
-          newUser.id = uuid.v4();
-          users.push(newUser);
-          res.status(201).json(newUser)
+        Users.findOne({ Username: req.body.Username })
+        .then((user) => {
+          if (user) {
+            return res.status(400).send(req.body.Username + 'already exists');
+          } else {
+            Users
+              .create({
+                Username: req.body.Username,
+                Password: req.body.Password,
+                Email: req.body.Email,
+                Birthday: req.body.Birthday
+              })
+              .then((user) =>{res.status(201).json(user) })
+            .catch((error) => {
+              console.error(error);
+              res.status(500).send('Error: ' + error);
+            })
           }
-          else {
-            res.status(400).send('new user not found')
-          }
-      });
+        })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).send('Error: ' + error);
+        });
+});
       
-      app.post('/users/:id/:favoriteMovieTitle', (req,res)=>{
-        const{id, favoriteMovieTitle}=req.params;
-      
-        let user=users.find(user=>user.id == id);
-      
-        if(user){
-            user.favoriteMovieList.push(favoriteMovieTitle);
-            res.status(201).send('movie added to your favorites list');
-            console.log(favoriteMovieTitle);
-        }else{
-            res.status(400).send('movie not added');
-        }
+      app.post('/users/:Username/movies/:id', (req,res)=> {
+        Users.findOneAndUpdate({ Username: req.params.Username }, {
+                $addToSet: { FavoriteMovies: req.params.id }},
+                req.body,
+                { new: true })
+                .then((updatedUser) => {
+                  res.status(200).json(updatedUser);
+                })
+                .catch(error => {
+                  res.status(500).json({ error: error.message });
+             });
       });
       
       
       //UPDATE
-      app.put('/users/:id', (req, res)=>{
-        const {id}=req.params;
-        const updateUser=req.body;
-      
-        let user = users.find(user => user.id === id );
-      
-        if(user){
-            user.name = updateUser.name;
-            res.status(200).json(user);
-        }else{
-            res.status(400).send('no such user');
-        }
+      app.put('/users/:Username', (req, res)=> {
+        Users.findOneAndUpdate({ Username: req.params.Username }, { $set:
+                {
+                  Username: req.body.Username,
+                  Password: req.body.Password,
+                  Email: req.body.Email,
+                  Birthday: req.body.Birthday
+                }
+              },
+              { new: true })
+              .then((updatedUser) => {
+                res.status(200).json(updatedUser);
+              })
+              .catch(error => {
+                res.status(500).json({ error: error.message });
+          });      
       });
       
       
-      //DELTE
-      app.delete('/users/:id/:favoriteMovieTitle', (req,res)=>{
-        const {id, favoriteMovieTitle} =req.params;
-      
-        let user = users.find(user=>user.id ==id);
-      
-        if(user){ user.favoriteMovieList=user.favoriteMovieList.filter(title=>title !== favoriteMovieTitle);
-            res.status(201).send('movie was deleted from your favorites');
-        }else{
-            res.status(400).send('movie was not deleted');
-        }
+      //DELETE
+      app.delete('/users/:Username', (req,res)=>{
+        Users.findOneAndRemove({ Username: req.params.Username })
+    .then((user) => {
+      if (!user) {
+        res.status(400).send(req.params.Username + ' was not found');
+      } else {
+        res.status(200).send(req.params.Username + ' was deleted.');
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
       });
       
-      app.delete('/users/:id', (req, res) => {
-        const {id} = req.params;
-
-        let user = users.find(user => user.id === id );
-
-  if (user) {
-    users = users.filter(user => user.id !== req.params.id);
-    res.status(201).send('User account ' + req.params.id + ' was deleted.');
-  }
+      app.delete('/users/:Username/movies/:id', (req, res) => {
+        Users.findOneAndUpdate({ Username: req.params.Username }, {
+                $pull: { FavoriteMovies: req.params.id }},
+                req.body,
+                { new: true })
+                .then((updatedUser) => {
+                res.status(200).json(updatedUser);
+                })
+                .catch(error => {
+                res.status(500).json({ error: error.message });
+             });
 });
 
 //morgan middleware error handling function
