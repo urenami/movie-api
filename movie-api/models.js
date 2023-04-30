3697478d685787bc6a0c23c3dd4e94644ac99e1c
urenami <122377715+urenami@users.noreply.github.com>
2023-04-30 12:21:47 -0400

    downgraded mongoose and fixed error model.js

--- movie-api/models.js
+++ movie-api/models.js
@@ -24,7 +24,7 @@ let userSchema = mongoose.Schema({
 });
 
 let Movie = mongoose.model('Movie', movieSchema);
-let User = mongoose.model('User', userSchema);
+let User = mongoose.model('Users', userSchema);
 
 module.exports.Movie = Movie;
 module.exports.User = User;
