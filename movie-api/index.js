3114b96dfb880d146bdbbd8fe62c33093cf404bb
urenami <122377715+urenami@users.noreply.github.com>
2023-05-22 17:50:56 -0400

    updated index.js

--- movie-api/index.js
+++ movie-api/index.js
@@ -50,7 +50,7 @@ const Models = require("./models.js");
 const Movies = Models.Movie;
 const Users = Models.User;
 
-mongoose.connect(process.env.CONNECTION_URI, {
+mongoose.connect( process.env.CONNECTION_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
